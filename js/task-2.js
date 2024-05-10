a3fae9192bc10eb007147f3377e933433bf9a158
sssBu4 <skleto2004denis@gmail.com>
2024-05-10 20:54:29 +0300

    +_+

--- js/task-2.js
+++ js/task-2.js
@@ -1,29 +1,34 @@
-function Storage(initialItems) {
-    this.items = initialItems;
-  
-    this.getItems = function() {
-      return this.items;
-    };
-  
-    this.addItem = function(newItem) {
-      this.items.push(newItem);
-    };
-  
-    this.removeItem = function(itemToRemove) {
-      this.items = this.items.filter(function(item) {
-        return item !== itemToRemove;
-      });
-    };
+class Storage {
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
   }
-  
-  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-  
-  storage.addItem("Droid");
-  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-  
-  storage.removeItem("Prolonger");
-  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-  
-  storage.removeItem("Scaner");
-  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,28 @@
 function StringBuilder(initialValue) {
-    this.value = initialValue;
-  
-    this.getValue = function() {
-      return this.value;
-    };
-  
-    this.padEnd = function(str) {
-      this.value += str;
-    };
-  
-    this.padStart = function(str) {
-      this.value = str + this.value;
-    };
-  
-    this.padBoth = function(str) {
-      this.value = str + this.value + str;
-    };
-  }
-  
-  const builder = new StringBuilder(".");
-  console.log(builder.getValue()); // "."
-  builder.padStart("^");
-  console.log(builder.getValue()); // "^."
-  builder.padEnd("^");
-  console.log(builder.getValue()); // "^.^"
-  builder.padBoth("=");
-  console.log(builder.getValue()); // "=^.^="
+  let value = initialValue;
+
+  this.getValue = function() {
+    return value;
+  };
+
+  this.padEnd = function(str) {
+    value += str;
+  };
+
+  this.padStart = function(str) {
+    value = str + value;
+  };
+
+  this.padBoth = function(str) {
+    value = str + value + str;
+  };
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
